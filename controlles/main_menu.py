01d93db40544f7f02d942bfd160adb6602602098
RaulA001 <95509340+RaulA001@users.noreply.github.com>
2022-02-09 21:55:10 -0300

    init menu de add base

--- controlles/main_menu.py
+++ controlles/main_menu.py
@@ -4,6 +4,21 @@ import json
 
 app = Blueprint('menu', __name__, static_folder='../static', template_folder='../template')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def menu():
     return render_template('main_menu.html')
+
+@app.route('/teste', methods=['GET', 'POST'])
+def test():
+    dados = dict(request.args)
+    dados = json.dumps(dados)
+    print(dados)
+    return render_template('test_menu.html')
+
+@app.route('/nova_nota', methods=['GET', 'POST'])
+def nova_nota():
+    return render_template('nova_nota.html')
+
+@app.route('/novo_teste', methods=['GET', 'POST'])
+def novo_teste():
+    return render_template('novo_teste.html')
